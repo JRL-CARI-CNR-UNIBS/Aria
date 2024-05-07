23966c84a5025c52f340efa110ce60faa076321b
MichiDelle <michi.delle07@gmail.com>
2024-05-07 15:53:50 +0200

    temp config

---
+++ config.sh
@@ -0,0 +1,13 @@
+echo "Run as Root"
+
+# Create directories
+sudo mkdir -p /usr/local/include/Aria/
+sudo mkdir -p /usr/local/include/ArNetworking/
+
+# Copy includes
+sudo cp -r include/* /usr/local/include/Aria/
+sudo cp -r ArNetworking/include/* /usr/local/include/ArNetworking/
+
+# Copy lib
+sudo cp lib/libAria.so /usr/local/lib/libAria.so
+sudo cp lib/libArNetworking.so /usr/local/lib/libArNetworking.so
